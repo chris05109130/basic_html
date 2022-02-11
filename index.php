6f3d1f7766ca88771e6f48a5704edca7e5c31470
chris05109130 <92364093+chris05109130@users.noreply.github.com>
2022-02-11 02:41:07 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Basic HTML</title>
+  <link rel="stylesheet" href="main.css">
+</head>
+<body>
+  <section id="Basic">
+    <h1 class="explain">Heading Tags, Heading One to Heading Six</h1>
+    <h1 class="explain">Makes text bold, and either bigger or smaller</h1>
+    <h1 class="example">Heading One</h1>
+    <h2 class="example">Heading Two</h2>
+    <h3 class="example">Heading Three</h3>
+    <h4 class="example">Heading Four</h4>
+    <h5 class="example">Heading Five</h5>
+    <h6 class="example">Heading Six</h6>
+
+    <h1 class="explain">Paragraph tag tells the browser you are making a paragraph</h1>
+    <p class="example">Paragraph</p>
+
+    <div id="example">
+    <p class="explain">A div represents a layer on the webpage</p>
+    <h1 class="example">Example of a Div</h1>
+    </div>
+    <div class="example">
+    <p class="explain">a class represents a CSS property that you can assign properties to</p>
+    <p class="example"> Example of a Class</p>
+    </div>
+
+    <p class="explain">Header tag is a semantic tag that defines a header for a document or section </p>
+    <header class="example">Header Tag</header>
+
+    <p class="explain">Nav tag is a semantic tag that defines a container for navigation links</p>
+    <nav class="example"> Nav Tag</nav>
+
+    <p class="explain">Section tag is a semantic tag that defines defines a section in a document</p>
+    <section class="example">Section Tag</section>
+
+    <p class="explain">Article tag is a semantic tag that self contained article</p>
+    <article class="example">Article Tag</article>
+
+    <p class="explain">Aside tag is a semantic tag that defines content content aside from content</p>
+    <aside class="example">Aside Tag</aside>
+
+    <p class="explain">Footer tag is a semantic tag that defines a footer for a document</p>
+    <footer class="example">Footer Tag</footer>
+
+  </section>
+</body>
+</html>
